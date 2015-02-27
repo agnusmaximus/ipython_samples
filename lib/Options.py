ceb0334f054a786a00ccaef72780c4296606eaac
Max <agnusmaximus@berkeley.edu>
2015-02-27 04:31:23 -0800

    ignore text

--- lib/Options.py
+++
@@ -1,7 +0,0 @@
-class Options:
-    def __init__(self, threshold_m, threshold_n, max_iteration, tolerance, num_pc):
-        self.threshold_m = threshold_m
-        self.threshold_n = threshold_n
-        self.max_iteration = max_iteration
-        self.tolerance = tolerance
-        self.num_pc = num_pc
